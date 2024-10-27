7a848fbecfed490fc45e119bcfe8b702b91ab7ef
Tiziano <tinianoalenny@gmail.com>
2024-10-27 17:26:41 -0300

    Empezando c#

---
+++ Cs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var cantidad = 10;
+
+        if (i < 0){
+
+            console.writeline('el nuimero es menor a cero');
+
+        } else {
+
+
+            console.writeline('el nuimero es meyor  a cero');
+
+        }
+
+
+         for (int i = 1; i <= cantidad; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Números en el array:");
+        foreach (var num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        
+      
+    }
+}
